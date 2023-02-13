ab504f976b3358da46b375874be641486d25f6d1
Ryan <ryragusa@gmail.com>
2023-02-13 09:20:19 -0600

    modified image locations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
         <nav>
-            <img src="/src/assets/images/airbnb-logo.png" alt="airbnb-logo" height="25px" />
+            <img src="/assets/airbnb-logo.png" alt="airbnb-logo" height="25px" />
         </nav>
     )
   }
